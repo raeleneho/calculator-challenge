21a96ed65af86dd5734e0dba3f4608ab9aa38ea5
Rae Ho <raeyean@email.com>
2023-08-20 21:22:53 +1000

    renamed tests

--- src/utils/helpers.spec.ts
+++ src/utils/helpers.spec.ts
@@ -2,26 +2,26 @@ import './helpers'
 import { expect, test } from 'vitest'
 import { compoundInterest,simpleInterest } from './helpers'
 
-test('Annually Compounding with Full Year', () => {
+test('Annual compounding with full year', () => {
   expect(compoundInterest(100, 5, 24, 12)).toBe(110.25)
 })
 
-test('Annually Compounding with Less than one pay period', () => {
+test('Annual Compounding with less than one pay period', () => {
   expect(compoundInterest(100, 5, 6, 12)).toBe(102.5)
 })
 
-test('Annual Compounding with Partial Pay Period (6 extra months)', () => {
+test('Annual compounding with partial pay period (6 extra months)', () => {
   expect(compoundInterest(100, 5, 18, 12)).toBe(107.62)
 })
 
-test('Annual Compounding with Partial Pay Period (2 extra months)', () => {
+test('Annual Compounding with partial pay period (2 extra months)', () => {
   expect(compoundInterest(100, 5, 26, 12)).toBe(111.17)
 })
 
-test('Monthly Compounding with full year', () => {
+test('Monthly compounding with full year', () => {
   expect(compoundInterest(100, 5, 24, 1)).toBe(110.49)
 })
 
-test('Simple Interest Rate', () => {
+test('Simple interest rate', () => {
   expect(simpleInterest(100, 5, 24)).toBe(110.00)
 })
